7106d23b52e1811b996259f95783ffe84c1c9fbc
asfaqjani916 <ashfaqjani916@gmail.com>
2023-12-26 16:57:50 +0530

    finished project

--- Para-Generator/script.js
+++ Para-Generator/script.js
@@ -1,17 +1,41 @@
-const wordcnt = document.getElementById('now');
+let wordcnt;
 const contain = document.querySelector('.container')
+
+const getData = ()=>{
+    wordcnt = document.getElementById('now');
+    generatePara();
+}
+createWord = (n)=>
+{
+    let txt = "";
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+
+    for(let i = 0;i<n;++i)
+    {
+        const random = (Math.random()*25).toFixed(0);
+        txt+= letters[random];
+    }
+    return txt;
+
+}
+
 generatePara =() =>
 {
     words = Number(wordcnt.value);
-
+    let data = "";
+    for(let i=0;i<words;++i)
+    {
+        const rand = (Math.random()*10).toFixed(0);
+        data += createWord(rand);
+        data +=" ";
+    }
     const para = document.createElement("p");
     
-    para.innerHTML = "sample paragraph";
+    para.innerHTML = data;
 
     para.setAttribute("class","para");
 
     contain.append(para);
 }
-generatePara();
-generatePara();
+
 
