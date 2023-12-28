5164f3e1fe4b084da86ecbbcd949aaff2abc439a
asfaqjani916 <ashfaqjani916@gmail.com>
2023-12-28 15:19:50 +0530

    finished project

--- TODO-list/main.js
+++ TODO-list/main.js
@@ -1,2 +1,22 @@
-const inputtable = document.querySelector(".input");
- 
+let inp = document.querySelector("input");
+let btn = document.querySelector("button");
+let val = "";
+let container = document.querySelector(".container");
+
+btn.addEventListener("click", () => {
+    val = inp.value;
+    let div = document.createElement("div");
+    let p = document.createElement("p");
+    p.innerText = val;
+    div.appendChild(p);
+    div.classList.add("tasks");
+    let delkey = document.createElement("button");
+    delkey.innerText = "-";
+    div.appendChild(delkey);
+    container.appendChild(div);
+    inp.value = "";
+    delkey.addEventListener("click", () => {
+        div.remove();
+    })
+});
+
