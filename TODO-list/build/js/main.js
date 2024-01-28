7e2845ccd672af02d4cab049d70ebd645ca28932
asfaqjani916 <ashfaqjani916@gmail.com>
2024-01-28 14:09:39 +0530

    added local storage and finished the project

--- TODO-list/build/js/main.js
+++ TODO-list/build/js/main.js
@@ -1,23 +1,44 @@
 "use strict";
 let inp = document.querySelector("input");
 let btn = document.querySelector("button");
-let val = "";
 let container = document.querySelector(".container");
-btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", () => {
-    val = inp === null || inp === void 0 ? void 0 : inp.value;
-    let div = document.createElement("div");
-    let p = document.createElement("p");
-    p.innerText = val;
-    div.appendChild(p);
-    div.classList.add("tasks");
-    let delkey = document.createElement("button");
-    delkey.innerText = "-";
-    div.appendChild(delkey);
-    container === null || container === void 0 ? void 0 : container.appendChild(div);
-    if (inp !== null) {
-        inp.value = "";
-    }
-    delkey.addEventListener("click", () => {
-        div.remove();
+let form = document.querySelector("form");
+let val = "";
+let tasks = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : [];
+showAll();
+function showAll() {
+    tasks.forEach((value, index) => {
+        const div = document.createElement("div");
+        div.setAttribute("class", "task");
+        let p = document.createElement("p");
+        p.innerText = value;
+        div.appendChild(p);
+        div.classList.add("tasks");
+        let delkey = document.createElement("button");
+        delkey.innerText = "-";
+        div.appendChild(delkey);
+        container === null || container === void 0 ? void 0 : container.appendChild(div);
+        delkey.addEventListener("click", () => {
+            removeAll();
+            tasks.splice(index, 1);
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+            showAll();
+        });
     });
+}
+;
+function removeAll() {
+    tasks.forEach(() => {
+        const div = document.querySelector(".task");
+        div === null || div === void 0 ? void 0 : div.remove();
+    });
+}
+form === null || form === void 0 ? void 0 : form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if ((inp === null || inp === void 0 ? void 0 : inp.value) !== undefined) {
+        removeAll();
+        tasks.push(inp === null || inp === void 0 ? void 0 : inp.value);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        showAll();
+    }
 });
--- TODO-list/src/main.ts
+++ TODO-list/src/main.ts
@@ -1,25 +1,62 @@
 let inp = document.querySelector("input");
-let btn = document.querySelector("button");
-let val:string|undefined= "";
-type valType = string;
-
+let btn  = document.querySelector("button");
 let container = document.querySelector(".container");
+let form = document.querySelector("form");
+type valType = string;
+ 
+let val = "";
 
-btn?.addEventListener("click", ():void => {
-    val = inp?.value;
-    let div = document.createElement("div");
-    let p= document.createElement("p");
-    p.innerText = val as valType;
-    div.appendChild(p);
-    div.classList.add("tasks");
-    let delkey = document.createElement("button");
-    delkey.innerText = "-";
-    div.appendChild(delkey);
-    container?.appendChild(div);
-    if(inp!==null){
-        inp.value = ""; 
+let tasks : string[] = localStorage.getItem("tasks")? JSON.parse(localStorage.getItem("tasks") as valType):[];
+
+
+showAll();
+
+function showAll():void{
+    tasks.forEach((value,index)=>
+    {
+        const div = document.createElement("div");
+        div.setAttribute("class","task");
+        let p= document.createElement("p");
+        
+         p.innerText = value as string;
+        div.appendChild(p);
+        div.classList.add("tasks");
+        let delkey = document.createElement("button");
+        delkey.innerText = "-";
+        div.appendChild(delkey);
+        container?.appendChild(div);
+
+        delkey.addEventListener("click",()=>{
+            removeAll();
+            tasks.splice(index,1);
+            localStorage.setItem("tasks",JSON.stringify(tasks));
+            showAll();
+        })
+    
+
+    })
+};
+
+
+
+function removeAll():void{
+    tasks.forEach(()=>
+    {
+        const div = document.querySelector(".task");
+        div?.remove();
+    });
+
     }
-    delkey.addEventListener("click", ():void => {
-        div.remove();
-    });
-    });
+
+form?.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    if(inp?.value !== undefined){
+        removeAll();
+        tasks.push(
+            inp?.value
+        );
+        localStorage.setItem("tasks",JSON.stringify(tasks));
+        showAll();
+    }
+    
+});
